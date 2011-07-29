svn2github/redmine

commit 61b8799407159842db3639c8c65e67c5acea6464

Fixed: MailHandler does not include JournalDetail for attached files (#7966).
git-svn-id: http://redmine.rubyforge.org/svn@6312 e93f8b46-1217-0410-a6f0-8f06a7374b81

trunk/app/models/attachment.rb
@@ -149,7 +149,8 @@ class Attachment < ActiveRecord::Base
                               :file => file,
                               :description => attachment['description'].to_s.strip,
                               :author => User.current)
-
+        obj.attachments << a
+        
         if a.new_record?
           obj.unsaved_attachments ||= []
           obj.unsaved_attachments << a

trunk/app/models/issue.rb
@@ -35,7 +35,7 @@ class Issue < ActiveRecord::Base
   has_many :relations_to, :class_name => 'IssueRelation', :foreign_key => 'issue_to_id', :dependent => :delete_all
 
   acts_as_nested_set :scope => 'root_id', :dependent => :destroy
-  acts_as_attachable :after_remove => :attachment_removed
+  acts_as_attachable :after_add => :attachment_added, :after_remove => :attachment_removed
   acts_as_customizable
   acts_as_watchable
   acts_as_searchable :columns => ['subject', "#{table_name}.description", "#{Journal.table_name}.notes"],
@@ -612,8 +612,6 @@ class Issue < ActiveRecord::Base
 
       if valid?
         attachments = Attachment.attach_files(self, params[:attachments])
-
-        attachments[:files].each {|a| @current_journal.details << JournalDetail.new(:property => 'attachment', :prop_key => a.id, :value => a.filename)}
         # TODO: Rename hook
         Redmine::Hook.call_hook(:controller_issues_edit_before_save, { :params => params, :issue => self, :time_entry => @time_entry, :journal => @current_journal})
         begin
@@ -840,6 +838,13 @@ class Issue < ActiveRecord::Base
         issue.fixed_version = nil
         issue.save
       end
+    end
+  end
+  
+  # Callback on attachment deletion
+  def attachment_added(obj)
+    if @current_journal && !obj.new_record?
+      @current_journal.details << JournalDetail.new(:property => 'attachment', :prop_key => obj.id, :value => obj.filename)
     end
   end
 

trunk/app/models/mail_handler.rb
@@ -200,7 +200,7 @@ class MailHandler < ActionMailer::Base
   def add_attachments(obj)
     if email.has_attachments?
       email.attachments.each do |attachment|
-        Attachment.create(:container => obj,
+        obj.attachments << Attachment.create(:container => obj,
                           :file => attachment,
                           :author => user,
                           :content_type => attachment.content_type)

trunk/test/unit/mail_handler_test.rb
@@ -307,7 +307,7 @@ class MailHandlerTest < ActiveSupport::TestCase
     assert_equal 1, ActionMailer::Base.deliveries.size
   end
 
-  def test_add_issue_note
+  def test_update_issue
     journal = submit_email('ticket_reply.eml')
     assert journal.is_a?(Journal)
     assert_equal User.find_by_login('jsmith'), journal.user
@@ -316,7 +316,7 @@ class MailHandlerTest < ActiveSupport::TestCase
     assert_equal 'Feature request', journal.issue.tracker.name
   end
 
-  def test_add_issue_note_with_attribute_changes
+  def test_update_issue_with_attribute_changes
     # This email contains: 'Status: Resolved'
     journal = submit_email('ticket_reply_with_status.eml')
     assert journal.is_a?(Journal)
@@ -334,15 +334,36 @@ class MailHandlerTest < ActiveSupport::TestCase
     assert !journal.notes.match(/^Status:/i)
     assert !journal.notes.match(/^Start Date:/i)
   end
+  
+  def test_update_issue_with_attachment
+    assert_difference 'Journal.count' do
+      assert_difference 'JournalDetail.count' do
+        assert_difference 'Attachment.count' do
+          assert_no_difference 'Issue.count' do
+            journal = submit_email('ticket_with_attachment.eml') do |raw|
+              raw.gsub! /^Subject: .*$/, 'Subject: Re: [Cookbook - Feature #2] (New) Add ingredients categories'
+            end
+          end
+        end
+      end
+    end
+    journal = Journal.first(:order => 'id DESC')
+    assert_equal Issue.find(2), journal.journalized
+    assert_equal 1, journal.details.size
+    
+    detail = journal.details.first
+    assert_equal 'attachment', detail.property
+    assert_equal 'Paella.jpg', detail.value
+  end
 
-  def test_add_issue_note_should_send_email_notification
+  def test_update_issue_should_send_email_notification
     ActionMailer::Base.deliveries.clear
     journal = submit_email('ticket_reply.eml')
     assert journal.is_a?(Journal)
     assert_equal 1, ActionMailer::Base.deliveries.size
   end
 
-  def test_add_issue_note_should_not_set_defaults
+  def test_update_issue_should_not_set_defaults
     journal = submit_email('ticket_reply.eml', :issue => {:tracker => 'Support request', :priority => 'High'})
     assert journal.is_a?(Journal)
     assert_match /This is reply/, journal.notes

commit 1a64b764585cc3ea36f8b303e0a9c50ba1c123af

Fixed: file uploads broken by r6312 (#8912).
git-svn-id: http://redmine.rubyforge.org/svn@6320 e93f8b46-1217-0410-a6f0-8f06a7374b81

trunk/app/models/attachment.rb
@@ -74,7 +74,7 @@ class Attachment < ActiveRecord::Base
   # and computes its MD5 hash
   def before_save
     if @temp_file && (@temp_file.size > 0)
-      logger.debug("saving '#{self.diskfile}'")
+      logger.info("Saving attachment '#{self.diskfile}' (#{@temp_file.size} bytes)")
       md5 = Digest::MD5.new
       File.open(diskfile, "wb") do |f|
         buffer = ""
@@ -85,6 +85,7 @@ class Attachment < ActiveRecord::Base
       end
       self.digest = md5.hexdigest
     end
+    @temp_file = nil
     # Don't save the content type if it's longer than the authorized length
     if self.content_type && self.content_type.length > 255
       self.content_type = nil

trunk/test/unit/attachment_test.rb
@@ -36,6 +36,7 @@ class AttachmentTest < ActiveSupport::TestCase
     assert_equal 0, a.downloads
     assert_equal '1478adae0d4eb06d35897518540e25d6', a.digest
     assert File.exist?(a.diskfile)
+    assert_equal 59, File.size(a.diskfile)
   end
 
   def test_create_should_auto_assign_content_type
@@ -64,7 +65,27 @@ class AttachmentTest < ActiveSupport::TestCase
     assert_equal 'cbb5b0f30978ba03731d61f9f6d10011', Attachment.disk_filename("test_accentué.ça")[13..-1]
   end
 
-  context "Attachmnet#attach_files" do
+  context "Attachmnet.attach_files" do
+    should "attach the file" do
+      issue = Issue.first
+      assert_difference 'Attachment.count' do
+        Attachment.attach_files(issue,
+          '1' => {
+            'file' => uploaded_test_file('testfile.txt', 'text/plain'),
+            'description' => 'test'
+          })
+      end
+      
+      attachment = Attachment.first(:order => 'id DESC')
+      assert_equal issue, attachment.container
+      assert_equal 'testfile.txt', attachment.filename
+      assert_equal 59, attachment.filesize
+      assert_equal 'test', attachment.description
+      assert_equal 'text/plain', attachment.content_type
+      assert File.exists?(attachment.diskfile)
+      assert_equal 59, File.size(attachment.diskfile)
+    end
+    
     should "add unsaved files to the object as unsaved attachments" do
       # Max size of 0 to force Attachment creation failures
       with_settings(:attachment_max_size => 0) do